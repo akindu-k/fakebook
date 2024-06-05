320ac2f0c71ed1cacba080c04a906394f61c462d
akindu-k <akinduk619@gmail.com>
2024-06-05 21:20:02 +0530

    feat: Add logo to NavBar and update search input class name

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -3,16 +3,17 @@ import SearchIcon from "@mui/icons-material/Search";
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
 import SettingsIcon from '@mui/icons-material/Settings';
+import './navBar.css';
 export default function NavBar() {
   return (
     <div className="navBarBox">
       <div className="navBarLeft">
-        <span>fakebook</span>
+        <span className="logo">fakebook</span>
       </div>
       <div className="navBarCenter">
         <div className="searchBarBox"></div>
         <SearchIcon className="searchIcon" />
-        <input placeholder="search for your friend" className="Input"/>
+        <input placeholder="search for your friend" className="searchInput"/>
       </div>
       <div className="navBarRight">
         <div className="navBarLinks">
